8b60ddb9c03b2004153813d5d4a025a2f3153fe6
Artenes Nogueira <artenesama@gmail.com>
2024-06-20 21:21:52 -0400

    Fix alignment for numbers

--- app/src/main/java/dev/artenes/template/app/features/TimerScreen.kt
+++ app/src/main/java/dev/artenes/template/app/features/TimerScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -16,17 +17,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.LifecycleResumeEffect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import timber.log.Timber
 
 @Composable
@@ -49,6 +55,7 @@ fun TimerScreen(
 
     Scaffold { edges ->
 
+
         Box(
             modifier = Modifier
                 .padding(edges)
@@ -64,16 +71,10 @@ fun TimerScreen(
             contentAlignment = Alignment.Center
         ) {
 
-            LazyRow(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
+            LazyRow(verticalAlignment = Alignment.CenterVertically) {
 
                 item {
-                    Digit(maxInclusive = 5)
-                }
-
-                item {
-                    Digit(maxInclusive = 9)
+                    Digit(maxInclusive = 59)
                 }
 
                 item {
@@ -81,11 +82,7 @@ fun TimerScreen(
                 }
 
                 item {
-                    Digit(maxInclusive = 5)
-                }
-
-                item {
-                    Digit(maxInclusive = 9)
+                    Digit(maxInclusive = 59)
                 }
 
             }
@@ -101,25 +98,44 @@ fun TimerScreen(
 @Composable
 private fun Digit(maxInclusive: Int) {
 
+    val coroutineScope = rememberCoroutineScope()
     val listState = rememberLazyListState()
+
+    LaunchedEffect(listState) {
+        snapshotFlow { listState.isScrollInProgress }
+            .filter { !it }
+            .distinctUntilChanged()
+            .collect {
+                Timber.d("FINISHED")
+            }
+    }
 
     LazyColumn(
         modifier = Modifier.height(100.dp),
         state = listState,
         flingBehavior = rememberSnapFlingBehavior(lazyListState = listState),
     ) {
-        items((maxInclusive + 1)) {
 
-            Text(
-                color = MaterialTheme.colorScheme.primary,
-                text = it.toString(),
-                style = MaterialTheme.typography.titleLarge.copy(
-                    fontSize = 100.sp,
-                    fontWeight = FontWeight.Light
-                ),
-            )
+        items((maxInclusive + 1)) { number ->
+
+            Box(
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(150.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    color = MaterialTheme.colorScheme.primary,
+                    text = String.format("%02d", number),
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 100.sp,
+                        fontWeight = FontWeight.Light
+                    ),
+                )
+            }
 
         }
+
     }
 
 }
@@ -128,21 +144,21 @@ private fun Digit(maxInclusive: Int) {
 fun Colon() {
     val color = MaterialTheme.colorScheme.primary
     Canvas(modifier = Modifier.size(50.dp)) {
-        val radius = 3.dp.toPx()
+        val radius = 5.dp.toPx()
         val spacing = 30.dp.toPx()
         val centerX = size.width / 2
-        val centerY1 = size.height / 2 - spacing / 2
-        val centerY2 = size.height / 2 + spacing / 2
+        val centerY1 = (size.height / 2 + spacing / 2) - spacing / 2
+        val centerY2 = (size.height / 2 + spacing / 2) + spacing / 2
 
         drawCircle(
             color,
             radius,
-            center = androidx.compose.ui.geometry.Offset(centerX, centerY1)
+            center = Offset(centerX, centerY1)
         )
         drawCircle(
             color,
             radius,
-            center = androidx.compose.ui.geometry.Offset(centerX, centerY2)
+            center = Offset(centerX, centerY2)
         )
     }
 }
